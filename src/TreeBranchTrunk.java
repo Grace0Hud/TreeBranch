afc5bfba692a74539d3b878beb484f65499a3189
Grace0Hud <gracein3d@gmail.com>
2020-03-09 13:29:10 -0700

    Added lorax message

--- src/TreeBranchTrunk.java
+++ src/TreeBranchTrunk.java
@@ -5,6 +5,7 @@ public class TreeBranchTrunk
         System.out.println("The trees are dying.");
         System.out.println("Because the world is warming.");
         System.out.println("The coral is dying.");
+        System.out.println("I am the lorax. I speak for the trees.");
     }//end main method
 
 }//end treeBranchTrunk (driver class)
