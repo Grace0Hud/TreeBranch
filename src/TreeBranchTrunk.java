177f6367c2648ebffe8893033c6a32775c8c1d8d
Grace0Hud <gracein3d@gmail.com>
2020-03-09 13:16:28 -0700

    Initial commit

---
+++ src/TreeBranchTrunk.java
@@ -0,0 +1,8 @@
+public class TreeBranchTrunk
+{
+    public static void main(String [] args)
+    {
+        System.out.println("The trees are dying.");
+    }//end main method
+
+}//end treeBranchTrunk (driver class)
