0e177882fba9ce436f5042244c91c60c3fda3b00
Grace0Hud <gracein3d@gmail.com>
2020-03-09 13:23:21 -0700

    Added warming message

--- src/TreeBranchTrunk.java
+++ src/TreeBranchTrunk.java
@@ -3,6 +3,7 @@ public class TreeBranchTrunk
     public static void main(String [] args)
     {
         System.out.println("The trees are dying.");
+        System.out.println("Because the world is warming.");
     }//end main method
 
 }//end treeBranchTrunk (driver class)
